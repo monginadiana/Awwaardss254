5c332faff10978f417d35515fc3ecafa2525e97f
monginadiana <diana.ondieki@student.moringa.com>
2021-12-10 21:53:31 +0300

    addition of the  class profile

--- Awards254/ratings/models.py
+++ Awards254/ratings/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_photo = CloudinaryField('image')
+    bio = models.TextField(max_length=500, blank=True, null=True)
+    contact = models.CharField(max_length=50, blank=True, null=True)
+
+
+    def save_profile(self):
+        self.save() 
+
+    def update_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def filter_by_id(cls, id):
+        profile = Profile.objects.filter(user = id).first()
+        return profile
+    
+
+    def str(self):
+        return self.user.username
--- Awards254/ratings/views.py
+++ Awards254/ratings/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+
 # Create your views here.
 
 def index(request):
     
  
     return render(request, 'all-awards/index.html', )
+
