b5f94d0f80f3c38ddadcf74c85445d39b0c6c2b7
monginadiana <diana.ondieki@student.moringa.com>
2021-12-13 22:20:03 +0300

    addition  of the urls.py for the api endpoints

--- ratings/urls.py
+++ ratings/urls.py
@@ -4,12 +4,16 @@ from ratings import views
 
 urlpatterns = [
     path('',views.index,name= 'index'),
+    path('accounts/profile/', views.index,name='index'),
     path('profile/', views.profile, name='profile'),
     path('upload/project/', views.upload_project, name = "upload"),
     path('create_profile/',views.create_profile,name = 'create_profile'),
     path('update_profile/<int:id>',views.update_profile, name='update_profile'),
     path('search/', views.search_project, name='search.post'),
     path("project/<int:project_id>/", views.project_details, name="project_details"),
+    path('rate/<int:id>',views.rate, name='rate'),
+    path('api/projects/', views.ProjectList.as_view()),
+    path('api/profiles/',views.ProfileList.as_view())
     
     
     ]
