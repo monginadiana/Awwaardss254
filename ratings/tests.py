dbfc0baa991b0e4037dc8e64ea53c61985efb3ff
monginadiana <diana.ondieki@student.moringa.com>
2021-12-14 11:34:14 +0300

    addition of the setup class

--- ratings/tests.py
+++ ratings/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.contrib.auth.models import User
+from django.test import TestCase
+from .models import Project, Profile
+
+# Create your tests here.
+
+
+class ProjectTestClass(TestCase):  # Project class test
+    def setUp(self):
+        # create a user
+        user = User.objects.create(
+            username="test_user", first_name="diana", last_name="mongina"
+        )
+
+        self.project = Project(
+            name="Test Project",
+            description="Test Description",
+            image="image.jpg",
+            user=user,
+        )
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.project, Project))
