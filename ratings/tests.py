20f15a59a06765ff195dcc60c81e41cc517488e3
monginadiana <diana.ondieki@student.moringa.com>
2021-12-14 15:19:28 +0300

    addition of the testcases for rating class

--- ratings/tests.py
+++ ratings/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 # Create your tests here.
 from django.contrib.auth.models import User
 from django.test import TestCase
-from .models import Project, Profile
+from .models import Project, Profile,Rating
 
 # Create your tests here.
 
@@ -63,3 +63,22 @@ class ProfileTestClass(TestCase):  # Profile class test
         self.profile.delete_profile()
         profiles = Profile.objects.all()
         self.assertTrue(len(profiles) == 0)
+    
+          
+class RatingTestClass(TestCase):  # Rating class test
+    def setUp(self):
+        # create a user
+        user = User.objects.create(
+            username="test_user", first_name="diana", last_name="mongina"
+        )
+
+        self.ratings = Rating(
+            design_rate="2",
+            user=user,
+            content_rate="3",
+        )
+        
+    def test_save_method(self):
+        self.ratings.save_rating()
+        rating = Rating.objects.all()
+        self.assertTrue(len(rating) > 0)
